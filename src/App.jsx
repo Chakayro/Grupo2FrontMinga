556165ab36d9416a1d092e226a34d198fdee2a37
Ricardo Camacho <ing.camacho29@gmail.com>
2025-05-15 19:08:27 -0600

    Se agrega un layout nuevo para formularios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import MainLayout from './Layouts/MainLayout'
+import FormLayout from './Layouts/FormLayout'
 import Home from './Pages/Home'
 import Login from './Pages/Users/Login'
 import Register from './Pages/Users/Register'
@@ -8,14 +9,23 @@ import Register from './Pages/Users/Register'
 function App() {
   
 const router = createBrowserRouter([
-  {path: "/", element: <MainLayout/>,
+  {
+    path: "/",
+    element: <MainLayout/>,
     children: [
       {path:"/", element: <Home/>},
       {path:"/home", element: <Home/>},
+      
+      //Aca iran el resto de las rutas que se vayan creando
+
+    ]
+  },{
+    path: "/",
+    element: <FormLayout/>,
+    children: [
       {path:"/login", element: <Login/>},
       {path:"/register", element: <Register/>},
       //Aca iran el resto de las rutas que se vayan creando
-
     ]
   }
 ])
